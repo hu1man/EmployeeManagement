cb04e01f40f176935b31ee9aa8a50f91404b1392
Vidun Dulmika <90890581+hu1man@users.noreply.github.com>
2025-09-21 16:29:40 +0530

    uploaded files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeApp.Core;
+using EmployeeManagement.src.EmployeeApp.Core.Interfaces;
+using EmployeeManagement.src.EmployeeApp.Data;
+using EmployeeManagement.src.EmployeeApp.Data.Repositories;
+using EmployeeManagement.src.EmployeeApp.UI;
+using System;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+
+namespace EmployeeManagement
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string conn = "Data Source=DESKTOP-RU25KP9;Initial Catalog=EmployeeDB;Persist Security Info=True;User ID=sa;Password=123;";
+
+            DbInitializer.Initialize(conn);
+            IEmployeeRepository repo = new AdoEmployeeRepository(conn);
+            Application.Run(new MainForm(repo));
+
+
+        }
+    }
+}
